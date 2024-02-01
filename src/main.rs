54fd305de0a596b14c19aa28126f685bd3ef4eb6
umutyalcinn <e200503021@stud.tau.edu.tr>
2024-02-02 00:11:39 +0300

    feat: render char by char. bug: non-ascii chars

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use std::{fs, io::{stdout, Write}, time::Duration};
+use std::{fs, io::{stdout, Write}, time::Duration, char};
 
 use crossterm::{
     queue, terminal::{EnterAlternateScreen, 
     LeaveAlternateScreen, Clear, 
-    ClearType, enable_raw_mode}, 
+    ClearType, enable_raw_mode, window_size, disable_raw_mode}, 
     event::{
         poll, read, Event, 
         KeyModifiers, KeyCode},
-        cursor::MoveTo, style::Print
+        cursor::{MoveTo, MoveToNextLine}, style::Print
     };
 
 use log::LevelFilter;
@@ -22,6 +22,10 @@ fn main() {
     let mut stdout = stdout();
 
     let mut buffer = Vec::<u8>::new();
+    let mut line_endings = Vec::<u16>::new();
+
+    let mut window_width = 0;
+    let mut window_height = 0;
 
     let mut data = fs::read("test/test.txt").unwrap();
 
@@ -65,26 +69,97 @@ fn main() {
                                 buffer.push(char_buffer[i]);
                             }
                         }
+                        (false, KeyCode::Enter) => {
+                            let mut char_buffer: [u8; 4] = [0; 4];
+                            '\n'.encode_utf8(&mut char_buffer);
+
+                            for i in 0..'\n'.len_utf8() {
+                                buffer.push(char_buffer[i]);
+                            }
+                        }
                         (_, _) => {
                             // TODO: handle keys
                         }
                     }
                 }
-                _ => {;
+
+                Event::Resize(columns, rows) => {
+                    window_width = columns;
+                    window_height = rows;
+                }
+                _ => {
                     // TODO: handle all events
                 }
             }
 
-            let text_to_render = String::from_utf8(buffer.clone()).unwrap();
+
+            let mut x:u16 = 0;
+            let mut y:u16 = 0;
 
             queue!(stdout, Clear(ClearType::All)).unwrap();
-            queue!(stdout, MoveTo(0, 0)).unwrap();
-            queue!(stdout, Print(text_to_render)).unwrap();
+            queue!(stdout, MoveTo(x, y)).unwrap();
+            line_endings.clear();
+
+            let mut i = 0;
+            while i < buffer.len() {
+
+                let char_byte = buffer.get(i).unwrap().clone();
+
+                let utf8_len = get_utf8_len_from_first_byte(char_byte);
+
+                // BUG: Bug when rendering non-ascii characters
+
+                let mut utf8_counter = 0;
+                let mut utf8_buffer = Vec::<u8>::new();
+
+                while utf8_counter < utf8_len {
+                    let byte = buffer.get(i).unwrap().clone();
+                    utf8_buffer.push(byte);
+                    utf8_counter += 1;
+                    i += 1;
+                } 
+
+                queue!(stdout, Print(format!("{:?}", utf8_buffer))).unwrap();
+
+                let ch = String::from_utf8(utf8_buffer).unwrap();
+
+                if ch == "\n" {
+                    line_endings.push(x);
+                    y += 1;
+                    queue!(stdout, MoveToNextLine(1)).unwrap();
+                    continue;
+                }
+                queue!(stdout, Print(ch)).unwrap();
+
+                x += 1;
+                if x >= window_width {
+                    line_endings.push(x);
+                    y += 1;
+                }
+
+                i += 1;
+            }
         }
 
 
         stdout.flush().unwrap();
     }
 
+    disable_raw_mode().unwrap();
     queue!(stdout, LeaveAlternateScreen).unwrap();
+}
+
+fn get_utf8_len_from_first_byte(byte: u8) -> u8 {
+    if (!byte & (1u8 << 7)) != 0 {
+        return 1;
+    }
+    else if (byte & (6u8 << 5)) != 0 {
+        return 2;
+    }
+    else if (byte & (30u8 << 3)) != 0 {
+        return 3;
+    }
+    else {
+        return 4;
+    }
 }
